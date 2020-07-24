5cae7a53baaddf1fb9b6033168040fa148206e2c
Mateusz Brzoska <mateuszbrzoska@seznam.cz>
2020-07-24 23:12:52 +0200

    files formatted

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const deleteBtn = document.querySelector(".btn-delete");
 const listContainer = document.querySelector(".list-container");
 const container = document.querySelector(".container");
 
+const localStorageList = JSON.parse(localStorage.getItem("taskArray"));
+const localStorageHeight = JSON.parse(localStorage.getItem("height"));
+const localStorageId = JSON.parse(localStorage.getItem("id"));
 
-const localStorageList = JSON.parse(localStorage.getItem('taskArray'));
-const localStorageHeight = JSON.parse(localStorage.getItem('height'));
-const localStorageId = JSON.parse(localStorage.getItem('id'));
-
-let taskArray = localStorage.getItem('taskArray') !== null ? localStorageList : [];
-let height = localStorage.getItem('height') !== null ? localStorageHeight : 500;
-let id = localStorage.getItem('id') !== null ? localStorageId : 0;
+let taskArray =
+  localStorage.getItem("taskArray") !== null ? localStorageList : [];
+let height = localStorage.getItem("height") !== null ? localStorageHeight : 500;
+let id = localStorage.getItem("id") !== null ? localStorageId : 0;
 
 init();
 
@@ -24,9 +24,9 @@ function addToList(e) {
   } else {
     const inputValue = input.value;
     const task = {
-        ID: id++,
-        text: inputValue
-    }
+      ID: id++,
+      text: inputValue,
+    };
     taskArray.push(task);
     height = height + 65;
     container.style.height = `${height}px`;
@@ -38,40 +38,36 @@ function addToList(e) {
 
 // Remove task from list
 function removeFromList(id) {
-    taskArray = taskArray.filter(element => element.ID !== id);
-    height = height - 65;
-    container.style.height = `${height}px`;
-    refreshList();
-    updateLocalStorage();
+  taskArray = taskArray.filter((element) => element.ID !== id);
+  height = height - 65;
+  container.style.height = `${height}px`;
+  refreshList();
+  updateLocalStorage();
 }
 
 // Refresh task list
 function refreshList() {
-    listContainer.innerHTML = '';
-    taskArray.forEach(element => {
-        const item = document.createElement('li');
-        item.classList.add('item');
-        item.classList.add(`${element.ID}`);
-        item.innerHTML = `${element.text} <button onclick="removeFromList(${element.ID})" class="btn-delete">delete</button>`;
-        listContainer.appendChild(item);
-    });
+  listContainer.innerHTML = "";
+  taskArray.forEach((element) => {
+    const item = document.createElement("li");
+    item.classList.add("item");
+    item.classList.add(`${element.ID}`);
+    item.innerHTML = `${element.text} <button onclick="removeFromList(${element.ID})" class="btn-delete">delete</button>`;
+    listContainer.appendChild(item);
+  });
 }
 
 // Update local storage
 function updateLocalStorage() {
-    localStorage.setItem('taskArray', JSON.stringify(taskArray));
-    localStorage.setItem('height', height);
-    localStorage.setItem('id', id);
+  localStorage.setItem("taskArray", JSON.stringify(taskArray));
+  localStorage.setItem("height", height);
+  localStorage.setItem("id", id);
 }
 
 function init() {
-    refreshList();
+  refreshList();
 }
 
 // Event listeners
 input.addEventListener("submit", addToList);
 addBtn.addEventListener("click", addToList);
-
-
-
-
