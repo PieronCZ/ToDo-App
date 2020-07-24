b91175dd62c8435f78f68df3d668559f7907335e
Mateusz Brzoska <mateuszbrzoska@seznam.cz>
2020-07-24 22:34:09 +0200

    Delete button functionality added

--- script.js
+++ script.js
@@ -4,34 +4,57 @@ const deleteBtn = document.querySelector(".btn-delete");
 const listContainer = document.querySelector(".list-container");
 const container = document.querySelector(".container");
 
-const taskArray = [];
-const height = 700;
+let taskArray = [];
+let height = 700;
+let id = 0;
 
-// Add task to list function
+
+// Add task to list
 function addToList(e) {
   e.preventDefault();
 
   if (input.value.trim() === "") {
     alert("Specify the taks!");
   } else {
-    taskArray.push(input.value);
+    const inputValue = input.value;
+    const task = {
+        ID: id++,
+        text: inputValue
+    }
+    taskArray.push(task);
     console.log(taskArray);
     height = height + 65;
     container.style.height = `${height}px`;
-    const inputValue = input.value;
     input.value = "";
-    refreshList(inputValue);
+    // const {ID, text} = task;
+    refreshList();
   }
 }
 
-// Refresh task list
-function refreshList(value) {
-  const item = document.createElement("li");
-  item.classList.add("item");
-  item.innerHTML = `${value} <button class="btn-delete">delete</button>`;
-  listContainer.appendChild(item);
+// Remove task from list
+function removeFromList(id) {
+    taskArray = taskArray.filter(element => element.ID !== id);
+    height = height - 65;
+    container.style.height = `${height}px`;
+    refreshList();
 }
 
-// Add task to list
+// Refresh task list
+function refreshList() {
+    listContainer.innerHTML = '';
+    taskArray.forEach(element => {
+        const item = document.createElement('li');
+        item.classList.add('item');
+        item.classList.add(`${element.ID}`);
+        item.innerHTML = `${element.text} <button onclick="removeFromList(${element.ID})" class="btn-delete">delete</button>`;
+        listContainer.appendChild(item);
+    });
+}
+
+// Event listeners
 input.addEventListener("submit", addToList);
 addBtn.addEventListener("click", addToList);
+
+
+
+
